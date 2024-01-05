98b0c5dbf79ef3f4ab3273e8e76ef40fda78de8a
mjavad1100 <m.javadkhodsiani@gmail.com>
2024-01-06 03:14:20 +0330

    ru

--- src/section/contact/ContactList.jsx
+++ src/section/contact/ContactList.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ContactItem from './ContactItem';
 import './ContactList.css';
-import { Container } from 'react-bootstrap';
 const ContactList = ({Contacts, deleteHandler}) => {
     return (
-        <Container fluid className='mycontainer'>
+        <div className='mycontainer'>
             <h3>در خواست ها</h3>
         {Contacts.length ? (<ul className='contacts'>
                 {Contacts.map(Contact =>  
@@ -12,7 +11,7 @@ const ContactList = ({Contacts, deleteHandler}) => {
                     )}
             </ul>) : (
             <p className='massage'>no contacts yet!!!</p>)}
-        </Container>
+        </div>
 
     );
 };
